d381167e731ffcd3dcfc9f1d7a340c890edd7a0c
ext_vvoevodkina <vivoe@yandex.ru>
2020-09-22 11:40:15 +0300

    feat: add Dream team task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw new CustomError('Not implemented');
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let array = members.map(el => {
+    if (typeof el === 'string') {
+      return el.split(" ").join("").substring(0, 1)
+    }
+  })
+  let result = array.join('').toUpperCase().split('').sort().join('');
+  return result
 };

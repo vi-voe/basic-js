15087a71718e5f8b920cb01eb45593935cb546c0
ext_vvoevodkina <vivoe@yandex.ru>
2020-11-01 19:56:44 +0300

    try

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return true
 };
